b3d3f05cda5bc1495c3602a91e7de8797f5ef40b
stavros <stavros@stavros-sin-Mac-mini.local>
2026-09-13 22:14:18 +0200

    Grupper UR98251-bilder etter stil

--- stavros/ur98251/script.js
+++ stavros/ur98251/script.js
@@ -1,17 +1,25 @@
-const newImages = [
-  "new-02.jpg", "new-04.jpg", "new-01.jpg", "new-07.jpg",
-  "new-09.jpg", "new-03.jpg", "new-05.jpg", "new-06.jpg",
-  "new-08.jpg", "new-10.jpg", "new-11.jpg", "new-12.jpg",
-  "new-13.jpg", "new-14.jpg", "new-15.jpg", "new-16.jpg"
+const coolImages = [
+  "new-02.jpg", "new-03.jpg", "new-04.jpg", "new-05.jpg",
+  "new-16.jpg", "new-07.jpg", "new-08.jpg", "new-10.jpg",
+  "new-12.jpg", "new-14.jpg", "new-15.jpg"
 ].map(file => `media/${file}`);
-const archiveImages = Array.from({ length: 50 }, (_, index) =>
-  `media/car-${String(index + 1).padStart(2, "0")}.jpg`
+const warmImages = [
+  "new-06.jpg", "new-09.jpg", "new-01.jpg", "new-11.jpg", "new-13.jpg"
+].map(file => `media/${file}`);
+const outdoorArchive = [1, 2, 3, 4, 5, 50].map(number =>
+  `media/car-${String(number).padStart(2, "0")}.jpg`
 );
-const images = [...newImages, ...archiveImages];
+const dealerArchive = Array.from({ length: 44 }, (_, index) =>
+  `media/car-${String(index + 6).padStart(2, "0")}.jpg`
+);
+const newImages = [...coolImages, ...warmImages];
+const images = [...newImages, ...outdoorArchive, ...dealerArchive];
 const imageCount = images.length;
 
-const currentGallery = document.querySelector("#currentGallery");
-const gallery = document.querySelector("#gallery");
+const coolGallery = document.querySelector("#coolGallery");
+const warmGallery = document.querySelector("#warmGallery");
+const outdoorGallery = document.querySelector("#outdoorGallery");
+const dealerGallery = document.querySelector("#dealerGallery");
 const dialog = document.querySelector("#lightbox");
 const lightboxImage = dialog.querySelector("img");
 const counter = dialog.querySelector(".counter");
@@ -35,10 +43,18 @@ function photoButton(src, index, eager = false) {
   return button;
 }
 
-newImages.forEach((src, index) =>
-  currentGallery.append(photoButton(src, index, index === 0))
+coolImages.forEach((src, index) =>
+  coolGallery.append(photoButton(src, index, index === 0))
 );
-images.forEach((src, index) => gallery.append(photoButton(src, index)));
+warmImages.forEach((src, index) =>
+  warmGallery.append(photoButton(src, coolImages.length + index))
+);
+outdoorArchive.forEach((src, index) =>
+  outdoorGallery.append(photoButton(src, newImages.length + index))
+);
+dealerArchive.forEach((src, index) =>
+  dealerGallery.append(photoButton(src, newImages.length + outdoorArchive.length + index))
+);
 
 function openImage(index) {
   activeIndex = (index + imageCount) % imageCount;
